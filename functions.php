e65e559b3aa4305e5002dab25575cf3f57994224
unknown <rafanercessian@gmail.com>
2020-05-23 15:31:10 -0300

    Aula02-01-Menu de navegação

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/*Registrando menu de navegação*/
+function alura_intercambios_registrando_menu(){
+    register_nav_menu(
+        'menu-navegacao',
+        'Menu navegação'
+    );
+}
+add_action('init','alura_intercambios_registrando_menu');
