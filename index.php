805ec8a39c68e9ca8c825644a0a3c413baff0c62
unknown <rafanercessian@gmail.com>
2020-05-29 16:22:43 -0300

    Loop wordpress

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php require_once 'header.php'?>
-<h1>Olá Mundo</h1>
+<?php
+
+if(have_posts()):
+    while(have_posts()): the_post();
+        the_post_thumbnail();
+        the_title();
+        the_content();
+    endwhile;
+endif;
+
+?>
 <?php require_once  'footer.php'?>
